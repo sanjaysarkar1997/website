e34a84b2a5819c15f2ee574cbf99b709ead804f7
Sanjay sarkar <sanjaysarkar430gmail.com>
2019-04-13 13:24:22 +0530

    Pictures

--- JS/script.js
+++ JS/script.js
@@ -40,7 +40,6 @@ $(window).on('load',function() {
             });
 
 
-
 /*=====Hover Link=====*/
 /*$('ul.nav li.dropdown').hover(function() {
   $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(200);
